fd8c7e0ea17d90b310e2d4425fe229a987305dec
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2020-03-29 23:11:30 +0700

    Returns 400 bad request when found invalid signature

--- lib/echo_bot/router.ex
+++ lib/echo_bot/router.ex
@@ -1,12 +1,27 @@
 defmodule EchoBot.Router do
   use Plug.Router
 
+  plug(Plug.Logger)
   plug(:match)
   plug(:dispatch)
 
   post "/callback" do
     # TODO(wingyplus): validate signature
+    handle_events(conn, validate_signature(conn))
+  end
+
+  defp validate_signature(conn) do
+    get_req_header(conn, "x-่line-signature")
+    |> List.first()
+    |> LineBot.Signature.validate()
+  end
+
+  defp handle_events(conn, :ok) do
     send_resp(conn, 200, Jason.encode!(%{}))
+  end
+
+  defp handle_events(conn, :invalid_signature) do
+    send_resp(conn, 400, Jason.encode!(%{"message" => "invalid signature"}))
   end
 
   match _ do
