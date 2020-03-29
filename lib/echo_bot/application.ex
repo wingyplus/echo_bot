7b16d4f3497a275720711dce6edf4017a528fe97
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2020-03-29 23:26:38 +0700

    Change HTTP port to avoid port collision with ngrok

--- lib/echo_bot/application.ex
+++ lib/echo_bot/application.ex
@@ -3,7 +3,7 @@ defmodule EchoBot.Application do
 
   def start(_type, _args) do
     children = [
-      {Plug.Cowboy, scheme: :http, plug: EchoBot.Router, options: [port: 4040]}
+      {Plug.Cowboy, scheme: :http, plug: EchoBot.Router, options: [port: 8080]}
     ]
 
     opts = [strategy: :one_for_one, name: EchoBot.Supervisor]
