c8df5e34785b17f3c2a8fdc0ba9edd6d48250db8
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2020-04-02 22:56:46 +0700

    Rename hash/2 to sign_signature/2

--- lib/line_bot/signature.ex
+++ lib/line_bot/signature.ex
@@ -7,13 +7,13 @@ defmodule LineBot.Signature do
   def validate(nil, _, _), do: :invalid_signature
 
   def validate(signature, body, channel_secret) do
-    validate(signature, hash(channel_secret, body))
+    validate(signature, sign_signature(channel_secret, body))
   end
 
   defp validate(signature, signature), do: :ok
   defp validate(_, _), do: :invalid_signature
 
-  defp hash(channel_secret, body) do
+  defp sign_signature(channel_secret, body) do
     :crypto.hmac(:sha256, channel_secret, body) |> Base.encode64()
   end
 end
